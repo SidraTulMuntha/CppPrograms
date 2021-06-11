a71051f6345a6b379e7ec3a65c604d704e27b062
ShauryaSarswat <shauryasarswat01@gmail.com>
2021-06-11 18:16:00 +0530

    Hello World Program
    Hello World Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//Pre processor 
+//Pre processor directive
+//header file
+using namespace std;
+//std:
+//objectified code
+int main()
+//executible function
+{
+    //declaration and definition
+    cout<<"Hello World";
+    //std::cout relation
+    //currrent definition
+    return 0;
+    //importance
+}
