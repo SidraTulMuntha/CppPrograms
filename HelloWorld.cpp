1d831146493242404f8aecc6463ed066ac5384a7
ShauryaSarswat <shauryasarswat01@gmail.com>
2021-06-12 12:08:24 +0530

    updates
    updates

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,10 +2,19 @@
 //Pre processor 
 //Pre processor directive
 //header file
+//input and output
+//a sequence of characters and bytes
+//buffer
+//cout for printing on monitor
+//cin for taking input from console / keyboard
+//cerr for printing error on the screen without buffer
+//clog for printing error with logs and contains buffer introduction
 using namespace std;
 //std:
 //objectified code
 int main()
+
+//return type  function name ()
 //executible function
 {
     //declaration and definition
